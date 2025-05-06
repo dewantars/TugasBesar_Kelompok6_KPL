158a39f656764f6278c00d975398e052cccb11eb
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2025-05-06 16:57:11 +0700

    add Runtime configuration

--- HikepassApp/Program.cs
+++ HikepassApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HikepassApp.Controller;
+using HikepassApp.Services;
+using HikepassApp.View;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("== SISTEM PEMESANAN TIKET PENDAKIAN MALABAR ==\n");
+
+        ConfigService.LoadConfig("config.json");
+        ConfigService.ShowAvailableTrails();
+
+        var view = new TicketView();
+        var controller = new TicketController(view);
+        controller.CreateTicket();
+
+        Console.WriteLine("\nTerima kasih telah menggunakan layanan kami.");
+    }
+}
