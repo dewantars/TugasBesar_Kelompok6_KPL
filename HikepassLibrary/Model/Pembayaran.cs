8f95b1dc3616b6ddec36d1680ee3fc09f7a27bb2
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2025-05-07 19:13:39 +0700

    create Library

---
+++ HikepassLibrary/Model/Pembayaran.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HikepassLibrary.Model
+{
+    class Pembayaran
+    {
+        public int Id { get; set; }
+        public int BookingId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+    }
+}
