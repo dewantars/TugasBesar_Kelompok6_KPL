902726e0b2339db3364a5206f629fefeaa1eb1a3
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2025-05-09 13:36:45 +0700

    add GUI

--- HikepassApp/Program.cs
+++ HikepassApp/Program.cs
@@ -1,20 +1,8 @@
-﻿using HikepassApp.Controller;
-using HikepassApp.Services;
-using HikepassApp.View;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("== SISTEM PEMESANAN TIKET PENDAKIAN MALABAR ==\n");
-
-        ConfigService.LoadConfig("config.json");
-        ConfigService.ShowAvailableTrails();
-
-        var view = new TicketView();
-        var controller = new TicketController(view);
-        controller.CreateTicket();
-
+        
         Console.WriteLine("\nTerima kasih telah menggunakan layanan kami.");
     }
 }
---
+++ HikepassForm/View/Dashboard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HikepassForm
+{
+    public partial class Dashboard: UserControl
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+        public void LoadPage(UserControl page)
+        {
+            this.Controls.Clear(); // Hapus konten sebelumnya
+            page.Dock = DockStyle.Fill;
+            this.Controls.Add(page);
+        }
+        public void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear(); // Hapus konten sebelumnya
+        }
+
+    }
+}
