f4234a17ef87fccd4354c4b4fd40b02bac96d0b8
tjhhh <muhammadfauzan292004@gmail.com>
2025-05-10 11:08:49 +0700

    tambah dhe dina

---
+++ HikepassLibrary/Service/MonitoringService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HikepassLibrary.Model;
+
+namespace HikepassLibrary.Service
+{
+    public class MonitoringService
+    {
+        private Dictionary<string, string> stateTable = new Dictionary<string, string>
+        {
+            { "paid_checkin", "checked_in" },
+            { "checked_in_checkout", "checked_out" }
+        };
+
+        public void HandleTransition(User user, string aksi)
+        {
+            string key = $"{user.Status}_{aksi}";
+            if (stateTable.ContainsKey(key))
+            {
+                user.Status = stateTable[key];
+                Console.WriteLine($"{user.Nama} berhasil {aksi}.");
+            }
+            else
+            {
+                Console.WriteLine($"Aksi {aksi} tidak valid untuk {user.Nama}. Status saat ini: {user.Status}");
+            }
+        }
+    }
+}
