e32fd412692dc76954a0a9c885db59f13b44b288
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2025-05-16 14:44:05 +0700

    Add Laporan to Library

--- HikepassLibrary/Model/Menu.cs
+++ HikepassLibrary/Model/Menu.cs
@@ -13,6 +13,7 @@ namespace HikepassLibrary.Model
             Console.WriteLine("Pilih Pengguna:");
             Console.WriteLine("1. Pengelola");
             Console.WriteLine("2. Pendaki");
+            Console.WriteLine("3. Exit");
             Console.Write("Pilih: ");
         }
        
@@ -20,10 +21,11 @@ namespace HikepassLibrary.Model
         {
             Console.WriteLine("==================== HikePass App ====================");
             Console.WriteLine("Selamat Datang Admin!");
-            Console.WriteLine("1. Riwayat Pendakian");
+            Console.WriteLine("1. Monitoring Pendaki");
             Console.WriteLine("2. Edit Informasi");
-            Console.WriteLine("3. Monitoring");
-            Console.WriteLine("4. Log Out");
+            Console.WriteLine("3. Lihat Laporan");
+            Console.WriteLine("4. Lihat Riwayat Pendakian");
+            Console.WriteLine("5. Log Out");
             Console.Write("Pilih: ");
         }
         public static void menuUser()
@@ -33,8 +35,9 @@ namespace HikepassLibrary.Model
             Console.WriteLine("1. Reservasi");
             Console.WriteLine("2. Tiket Saya");
             Console.WriteLine("3. Lihat Informasi");
-            Console.WriteLine("4. Edit Profil");
-            Console.WriteLine("5. Log Out");
+            Console.WriteLine("4. Input Laporan");
+            Console.WriteLine("5. Edit Profil");
+            Console.WriteLine("6. Log Out");
             Console.Write("Pilih: ");
         }
         public static void menuTiketSaya() 
