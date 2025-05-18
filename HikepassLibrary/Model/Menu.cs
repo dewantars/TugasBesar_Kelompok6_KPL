cc2c63c046b663df74e1f2435b9823c8a6e343c8
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2025-05-18 14:03:29 +0700

    update

--- HikepassLibrary/Model/Menu.cs
+++ HikepassLibrary/Model/Menu.cs
@@ -62,18 +62,6 @@ namespace HikepassLibrary.Model
             Console.WriteLine("2. Puncak Besar Malabar Via Panorama(Pendek) 20k/orang");
             Console.WriteLine("Lanjutkan reservasi y/n?");
         }
-        public static void TampilkanData(RiwayatPendakianConfig data)
-        {
-            Console.WriteLine($"Tanggal Reservasi         : {data.tanggal_reservasi}");
-            Console.WriteLine($"Jumlah Pendaki          : {data.jumlah_reservasi}");
-            Console.WriteLine($"Jalur Pendakian           : {data.jalur_pendakian}");
-            Console.WriteLine($"Tanggal Pembayaran        : {data.tanggal_pembayaran}");
-            Console.WriteLine($"Metode Pembayaran         : {data.metode_pembayaran}");
-            Console.WriteLine($"Total Pembayaran          : Rp{data.total_pembayaran}");
-            Console.WriteLine($"Tanggal Check-In          : {data.tanggal_checkin}");
-            Console.WriteLine($"Laporan Sampah Check-In   : {data.laporan_sampah_checkin}");
-            Console.WriteLine($"Tanggal Check-Out         : {data.tanggal_checkout}");
-            Console.WriteLine($"Laporan Sampah Check-Out  : {data.laporan_sampah_checkout}");
-        }
+        
     }
 }
